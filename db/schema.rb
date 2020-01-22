41f8eaa694cc3dbf21b2abdd2076ca4cc5507f33
komstyle <59528384+komstyle@users.noreply.github.com>
2020-01-22 16:38:12 +0900

    add/devise-view-model_1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200122061704) do
+ActiveRecord::Schema.define(version: 20200122073002) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -21,4 +21,23 @@ ActiveRecord::Schema.define(version: 20200122061704) do
     t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
   end
 
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                                              null: false
+    t.text     "goal",                   limit: 65535
+    t.integer  "border"
+    t.integer  "group_id"
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["group_id"], name: "index_users_on_group_id", using: :btree
+    t.index ["name"], name: "index_users_on_name", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  add_foreign_key "users", "groups"
 end
