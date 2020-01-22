6c1bca37f6e93b3dc70679924f19d491f570c34b
komstyle <59528384+komstyle@users.noreply.github.com>
2020-01-22 15:37:20 +0900

    add/model-group_2

--- db/migrate/20200122061704_create_groups.rb
+++ db/migrate/20200122061704_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :name, null: false
-      t.index :name, unique: true
+      t.index :name
       t.string :type
       t.integer :flag
       t.timestamps
