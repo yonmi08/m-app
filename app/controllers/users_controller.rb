c1756dc9d6cab597c7d17ff7a9340633444c356f
komstyle <59528384+komstyle@users.noreply.github.com>
2020-01-23 19:31:42 +0900

    add/route-controller_1
    route.rbを編集
    controllerのdiariesとusersを編集。
    diaries（view）/new,editを追加。
    users（view）/index,editを追加。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user
+  
+  def index
+
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+
+  private
+  def user_params
+  end
+
+  def set_user
+
+  end
 end
