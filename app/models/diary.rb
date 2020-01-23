60c1efc8a0e7247f354edce321dcc03929765839
komstyle <59528384+komstyle@users.noreply.github.com>
2020-01-23 17:35:56 +0900

    add/association_1

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,2 +1,5 @@
 class Diary < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+  validates :text, :date, presence: true
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
-  
+  has_many :users
+  has_many :messages
+  has_many :diaries
+  validates :name, :type, :border, presence: true
 end
