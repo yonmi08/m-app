6085f2a2b3791bad3f1c5a22422fac843d83ae51
reyon08 <sorairo.benimairoi@gmail.com>
2020-01-24 19:24:15 +0900

    [WIP]controller view
    viewに関する記述を変更しました

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,37 @@
 class GroupsController < ApplicationController
+  def index
+  end
+
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def create
+    @group = Group.new(group_params)
+    # if @group.save # とりあえず、まだ条件分岐をつけずに単純化させています
+    #   redirect_to root_path, notice: 'グループを作成しました'
+    # else
+    #   render :new
+    # end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+
+  end
+  
+  def destroy
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, :genre, :border, :flag, user_ides: []) # usersは後からも入るため空で設定
+  end
+
 end
