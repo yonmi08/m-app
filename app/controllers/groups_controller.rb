01d029d110c8a314463ce396b0c63e2c530bf894
reyon08 <sorairo.benimairoi@gmail.com>
2020-01-25 23:24:39 +0900

    views-new/edit
    new/editのviewファイルを作成しました

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,11 +4,11 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @group.users << current_user
+    # @group.users << current_user
   end
 
   def create
-    @group = Group.new(group_params)
+    @group = Group.create(group_params)
     # if @group.save # とりあえず、まだ条件分岐をつけずに単純化させています
     #   redirect_to root_path, notice: 'グループを作成しました'
     # else
@@ -31,7 +31,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, :genre, :border, :flag, user_ides: []) # usersは後からも入るため空で設定
+    params.require(:group).permit(:name, :genre, :border, :flag) # usersは後からも入るため空で設定
   end
 
 end
