c32f1e6289325d360c8d1069ed3d80e6f4fe8a50
komstyle <59528384+komstyle@users.noreply.github.com>
2020-01-27 12:02:31 +0900

    add/user-diary_view_1
    確認をお願いします。

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,11 +1,19 @@
 class DiariesController < ApplicationController
+  before_action :set_group, only: [:new, :create, :edit, :update]
+  before_action :set_user, only: [:new, :create, :edit, :update, :show]
+  before_action :set_diary, only: [:edit, :update, :show]
 
   def new
-
+    @diary = Diary.new
   end
 
   def create
-    
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to root_path
+    else
+      render :new, alert: '正しく入力をしてください。'
+    end
   end
 
   def edit
@@ -13,6 +21,35 @@ class DiariesController < ApplicationController
   end
 
   def update
+    if @diary.update(diary_params)
+      redirect_to root_path
+    else
+      render :edit, alert: '正しく入力をしてください。'
+    end
+  end
 
+  def destroy
+    
+  end
+
+  def show
+
+  end
+
+  private
+  def diary_params
+    params.require(:diary).permit(:text, :point, :date).merge(user_id: current_user.id, group_id:@group.id )
+  end
+
+  def set_group
+    @group = Group.find(current_user.group_id)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_diary
+    @diary = Diary.find(params[:id]) 
   end
 end
--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,4 +2,5 @@ class Diary < ApplicationRecord
   belongs_to :group
   belongs_to :user
   validates :text, :date, presence: true
+  validates :user_id, :uniqueness => {:scope => :date}
 end
