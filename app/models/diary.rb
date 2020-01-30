56d1ece9ccb9d4ec0d75c398389d794ecdc9e155
komstyle <59528384+komstyle@users.noreply.github.com>
2020-01-30 16:57:23 +0900

    add/error-message_2
    グループ-new,edit
    diary-new,editの
    エラーメッセージを追加しました。

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,7 +1,7 @@
 class Diary < ApplicationRecord
   belongs_to :user
   validates :text, :date, presence: true
-  validates :user_id, :uniqueness => {:scope => :date}
-
+  validates :date, :uniqueness => {:scope => :user_id}
+    
   scope :date_sort, -> {order(date: :desc)}
 end
