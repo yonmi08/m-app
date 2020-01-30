8275b24fbfb481c371a516c07433f55680bef1a4
komstyle <59528384+komstyle@users.noreply.github.com>
2020-01-30 19:49:23 +0900

    add/group-controller_1

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   has_many :messages, dependent: :destroy
-  has_one :event, inverse_of: :group
+  has_one :event, inverse_of: :group, dependent: :destroy
   accepts_nested_attributes_for :event, allow_destroy: true
 
   validates :name, :genre, :border, presence: true
