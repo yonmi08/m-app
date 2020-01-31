c2d6d0588f5f39ca2627364421f4dc30553d2ee8
reyon08 <sorairo.benimairoi@gmail.com>
2020-01-31 13:28:09 +0900

    [WIP]test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  
   devise_for :users
   root 'users#index'
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
@@ -13,4 +12,3 @@ Rails.application.routes.draw do
     resources :events, only: [:index]
   end
 end
-a
