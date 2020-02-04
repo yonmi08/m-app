dd0a4c09ec705a1805cde766f53146ec59be18d4
komstyle <59528384+komstyle@users.noreply.github.com>
2020-02-04 12:22:29 +0900

    add/responsive_1

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,6 +1,6 @@
 class Diary < ApplicationRecord
   belongs_to :user
-  validates :text, :date, presence: true
+  validates :text, :date, :point, presence: true
   validates :date, :uniqueness => {:scope => :user_id}
     
   scope :date_sort, -> {order(date: :desc)}
